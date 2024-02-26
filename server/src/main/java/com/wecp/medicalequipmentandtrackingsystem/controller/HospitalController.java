f360fed9902f0f7955181993186faeeda9cf2c21
username <wecp-dummy@gmail.com>
2024-02-26 07:17:36 +0000

    Nayaz did second Changes in Hospital Controller

--- server/src/main/java/com/wecp/medicalequipmentandtrackingsystem/controller/HospitalController.java
+++ server/src/main/java/com/wecp/medicalequipmentandtrackingsystem/controller/HospitalController.java
@@ -1,6 +1,8 @@
 package com.wecp.medicalequipmentandtrackingsystem.controller;
 
 
+
+//Nayaz added code in the Hospital Controller
 import com.wecp.medicalequipmentandtrackingsystem.entitiy.Equipment;
 import com.wecp.medicalequipmentandtrackingsystem.entitiy.Hospital;
 import com.wecp.medicalequipmentandtrackingsystem.entitiy.Maintenance;
